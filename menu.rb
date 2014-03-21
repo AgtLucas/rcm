6e372a352b7b4585998765998ab19b2d5df4db13
Lucas <lucas.silva@agenciamilagro.com.br>
2014-03-21 16:19:37 -0300

    Update menu

---
+++ menu.rb
@@ -0,0 +1,21 @@
+# Menu class
+
+class Menu
+
+  def self.coffee()
+    puts "Coffee"
+  end
+
+  def self.coffeeWithSugar()
+    puts "Coffee with sugar"
+  end
+
+  def self.coffeeWithCream()
+    puts "Coffee with cream"
+  end
+
+  def self.coffeeWithCreamAndSugar()
+    puts "Coffee with cream and sugar"
+  end
+
+end
